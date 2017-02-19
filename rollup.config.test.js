96dc7cd3ab2c6f26e010ea204afbae8f80e826c1
Thodoris Greasidis <thgreasi@gmail.com>
2017-02-19 12:09:42 +0200

    fix(rollup.config.test): use babelrc-rollup

--- rollup.config.test.js
+++ rollup.config.test.js
@@ -1,9 +1,10 @@
 import babel from 'rollup-plugin-babel';
+import babelrc from 'babelrc-rollup';
 import multiEntry from 'rollup-plugin-multi-entry';
 
 export default {
   entry: 'test/**/*_test.js',
-  plugins: [babel(), multiEntry()],
+  plugins: [babel(babelrc()), multiEntry()],
   format: 'cjs',
   intro: 'require("source-map-support").install();',
   dest: 'build/test-bundle.js',
