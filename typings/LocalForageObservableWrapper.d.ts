65c34c87e9e96613019795db3c185b59157d98dc
Thodoris Greasidis <thgreasi@gmail.com>
2017-03-18 10:38:30 +0200

    fix(LocalForageObservableWrapper): fix typing references

--- typings/LocalForageObservableWrapper.d.ts
+++ typings/LocalForageObservableWrapper.d.ts
@@ -1,8 +1,9 @@
-/// <reference path="LocalForageObservable.d.ts" />
+/// <reference path="LocalForageObservableChange.d.ts" />
+/// <reference path="LocalForageObservableOptions.d.ts" />
 /// <reference path="Observable.d.ts" />
 
 interface LocalForageObservableWrapper {
-    constructor(options: LocalForageObservableOptions, subscriptionObserver: SubscriptionObserver);
+    constructor(options: LocalForageObservableOptions, subscriptionObserver: SubscriptionObserver<any>);
     hasMethodFilterOptions(): boolean;
     publish(publishObject: LocalForageObservableChange): void;
 }
