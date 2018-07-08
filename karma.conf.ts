31115576ca3e954910f94ad2b9ac3c1fe95e0740
Thodoris Greasidis <thgreasi@gmail.com>
2018-07-08 10:12:09 +0300

    add tslint-no-unused-expression-chai & lint & format karma.conf

--- karma.conf.ts
+++ karma.conf.ts
@@ -1,29 +1,31 @@
+/* tslint:disable:no-var-requires */
 const path = require('path');
 const packageJSON = require('./package.json');
 const getKarmaConfig = require('resin-config-karma');
 
 const LF_MODULE_PATH = './node_modules/localforage';
 const LF_BROWSER_BUNDLE = path.join(
-  LF_MODULE_PATH,
-  require(`${LF_MODULE_PATH}/package.json`).main,
+    LF_MODULE_PATH,
+    require(`${LF_MODULE_PATH}/package.json`).main,
 );
-const ZEN_OBSERVABLE_BROWSER_BUNDLE = './node_modules/zen-observable/zen-observable.js';
+const ZEN_OBSERVABLE_BROWSER_BUNDLE =
+    './node_modules/zen-observable/zen-observable.js';
 const BROWSER_BUNDLE = packageJSON.main;
 
-module.exports = (config) => {
-  const karmaConfig = getKarmaConfig(packageJSON);
-  karmaConfig.webpack.node = {
-    global: true,
-    fs: 'empty',
-    dns: 'empty',
-    net: 'empty',
-    process: 'mock',
-  };
-  karmaConfig.files = [
-    LF_BROWSER_BUNDLE,
-    ZEN_OBSERVABLE_BROWSER_BUNDLE,
-    BROWSER_BUNDLE,
-    'build/test-e2e/**/*.spec.js',
-  ];
-  config.set(karmaConfig);
+module.exports = config => {
+    const karmaConfig = getKarmaConfig(packageJSON);
+    karmaConfig.webpack.node = {
+        global: true,
+        fs: 'empty',
+        dns: 'empty',
+        net: 'empty',
+        process: 'mock',
+    };
+    karmaConfig.files = [
+        LF_BROWSER_BUNDLE,
+        ZEN_OBSERVABLE_BROWSER_BUNDLE,
+        BROWSER_BUNDLE,
+        'build/test-e2e/**/*.spec.js',
+    ];
+    config.set(karmaConfig);
 };
