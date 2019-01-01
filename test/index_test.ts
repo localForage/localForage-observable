36fe784b507b94114d769296be59052564b5a6d5
Thodoris Greasidis <thgreasi@gmail.com>
2019-01-01 17:58:16 +0200

    Drop the empty unit tests

--- test/index_test.ts
+++
@@ -1,9 +0,0 @@
-import * as m from 'mochainon';
-
-describe('First test', function() {
-    it('works as expected', function() {
-        return m.chai
-            .expect(Promise.resolve(true))
-            .to.eventually.deep.equal(true);
-    });
-});
